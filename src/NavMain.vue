b550684a12b65f76467ac8193549110a341dbd34
[your <tw841117@gmail.com>
2023-11-02 11:59:48 -0400

    upload NavMain.vue

---
+++ src/NavMain.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import {ref} from "vue";
+    let text=ref("Main content");
+    let change=function(){
+        text.value="New content";
+    }
+</script>
+
+<template>
+    <main>
+        <div @click="change">{{ text }}</div>
+    </main>
+</template>
+
+<style scoped>
+    main{padding:10px; background-color:gray}
+</style>
